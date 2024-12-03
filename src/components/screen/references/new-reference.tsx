938092f0dc84f8895d38b5ba1779fc13071b31a3
Shiran Abbasi <shiran.abbasi@gmail.com>
2024-12-04 01:53:58 +0500

    reference completed

--- src/components/screen/references/new-reference.tsx
+++ src/components/screen/references/new-reference.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CSButton from "../../theme/atoms/cs-button";
 import { Form, Modal, Spin } from "antd";
 import CSInput from "../../theme/atoms/cs-input";
@@ -10,10 +10,15 @@ import usePostApi from "../../../hooks/use-post-api";
 import { API_ROUTES } from "../../../utils/constants/api-routes.constant";
 import useGetApi from "../../../hooks/use-get-api";
 import { IApiResponse } from "../../../utils/interface/response.interface";
+import { AnyObject } from "antd/es/_util/type";
+import CSCheckbox from "../../theme/atoms/cs-checkbox";
+import { useParams } from "react-router-dom";
+import { checkEditablePage } from "../../../utils/helper/general.helper";
 
 const NewReference = () => {
   const [categoryForm] = useForm();
   const [form] = useForm();
+  const params = useParams();
   const [category, setCategory] = useState<boolean>(false);
 
   const handleCatgory = () => {
@@ -24,13 +29,24 @@ const NewReference = () => {
     mutateCategory(values);
   };
 
-  const { data, isLoading, refetch } = useGetApi<
-    IApiResponse<{ id: string; name: string }[]>
-  >({
+  /** GET Reference Category API */
+  const {
+    data: categoryData,
+    isLoading: categoryLoading,
+    refetch,
+  } = useGetApi<IApiResponse<{ id: string; name: string }[]>>({
     key: [API_ROUTES.reference.getCategory],
     url: API_ROUTES.reference.getCategory,
   });
 
+  /** GET Reference API */
+  const { data, isLoading, isSuccess } = useGetApi<IApiResponse<AnyObject>>({
+    key: [API_ROUTES.reference.getById(params.id!), params.id],
+    url: API_ROUTES.reference.getById(params.id!),
+    enabled: Boolean(params?.id !== "new"),
+  });
+
+  /** POST Reference API */
   const { mutate, isPending } = usePostApi({
     url: API_ROUTES.reference.post,
     showSuccessMessage: true,
@@ -39,6 +55,14 @@ const NewReference = () => {
     },
   });
 
+  /** PUT Reference API */
+  const { mutate: updateMutate, isPending: updatePending } = usePostApi({
+    url: API_ROUTES.reference.post,
+    showSuccessMessage: true,
+    onSuccess: () => {},
+  });
+
+  /** POST Reference Category API */
   const { mutate: mutateCategory, isPending: categoryPending } = usePostApi({
     url: API_ROUTES.reference.postCategory,
     showSuccessMessage: true,
@@ -49,8 +73,15 @@ const NewReference = () => {
     },
   });
 
+  useEffect(() => {
+    if (isSuccess) {
+      form.setFieldsValue(data.data);
+      data.data;
+    }
+  }, [isSuccess]);
+
   const submitReference = (values: FormData) => {
-    mutate(values);
+    params?.id !== "new" ? updateMutate(values) : mutate(values);
   };
 
   return (
@@ -58,44 +89,55 @@ const NewReference = () => {
       <div className="category">
         <CSButton onClick={handleCatgory}>Add Category</CSButton>
       </div>
-      {/* <Spin spinning={false}> */}
       <div className="ref-form">
-        <Form form={form} layout="vertical" onFinish={submitReference}>
-          <CSFormItem
-            label="Name"
-            name={"name"}
-            rules={[{ required: true, message: "Name is required" }]}
-          >
-            <CSInput placeholder="Name" />
-          </CSFormItem>
-          <CSFormItem
-            label="Category"
-            name={"categoryId"}
-            rules={[{ required: true, message: "Category is required" }]}
-          >
-            <CSSelect
-              placeholder="Category"
-              loading={isLoading}
-              options={data?.data?.map((x) => ({
-                label: x.name,
-                value: x.id,
-              }))}
-            />
-          </CSFormItem>
-          <CSFormItem
-            label="Description"
-            name={"value"}
-            rules={[{ required: true, message: "Description is required" }]}
-          >
-            <CSTextarea rows={4} placeholder="Referece Description" />
-          </CSFormItem>
+        <Spin spinning={isLoading}>
+          <Form form={form} layout="vertical" onFinish={submitReference}>
+            <CSFormItem
+              label="Name"
+              name={"name"}
+              rules={[{ required: true, message: "Name is required" }]}
+            >
+              <CSInput placeholder="Name" />
+            </CSFormItem>
+            <CSFormItem
+              label="Category"
+              name={"categoryId"}
+              rules={[{ required: true, message: "Category is required" }]}
+            >
+              <CSSelect
+                placeholder="Category"
+                loading={categoryLoading}
+                options={categoryData?.data?.map((x) => ({
+                  label: x.name,
+                  value: x.id,
+                }))}
+              />
+            </CSFormItem>
+            <CSFormItem
+              label="Description"
+              name={"value"}
+              rules={[{ required: true, message: "Description is required" }]}
+            >
+              <CSTextarea rows={4} placeholder="Referece Description" />
+            </CSFormItem>
+            <CSFormItem name={"isSiteNote"} valuePropName="checked">
+              <CSCheckbox>Include in Site Note.</CSCheckbox>
+            </CSFormItem>
 
-          <CSButton type="primary" htmlType="submit" loading={isPending}>
-            {"Create"}
-          </CSButton>
-        </Form>
+            <CSButton
+              type="primary"
+              htmlType="submit"
+              loading={isPending || updatePending}
+            >
+              {checkEditablePage(
+                params?.id,
+                "Create Reference",
+                "Update Reference"
+              )}
+            </CSButton>
+          </Form>
+        </Spin>
       </div>
-      {/* </Spin> */}
       <Modal
         rootClassName="new-reference"
         footer={null}
